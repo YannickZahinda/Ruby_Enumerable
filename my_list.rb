8b4f88944cc04d4858c6fc9cdbaf03f237afb68f
Fsher07 <furkansalimm@gmail.com>
2022-06-18 11:34:14 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -1,7 +1,6 @@
 require_relative './enumerable'
 
 class MyList
-  
   include Enumerable
 
   def initialize(*array)
@@ -11,17 +10,16 @@ class MyList
   def each(&block)
     @list.each(&block)
   end
-
 end
 
 list = MyList.new(1, 2, 3, 4)
 
-p list.all? {|e| e < 5}
+p(list.all? { |e| e < 5 })
 
-p list.all? {|e| e > 5}
+p(list.all? { |e| e > 5 })
 
-p list.any? {|e| e == 2}
+p(list.any? { |e| e == 2 })
 
-p list.any? {|e| e == 5}
+p(list.any? { |e| e == 5 })
 
-p list.filter {|e| e.even?}
+p(list.filter(&:even?))
