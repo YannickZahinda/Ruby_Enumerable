03b7e4f336ec4dbd6a2194a2fae55997df3d8abd
Fsher07 <furkansalimm@gmail.com>
2022-06-18 11:29:10 +0300

    Add my list class

--- my_list.rb
+++ my_list.rb
@@ -1,1 +1,27 @@
+require_relative './enumerable'
 
+class MyList
+  
+  include Enumerable
+
+  def initialize(*array)
+    @list = array
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+p list.all? {|e| e < 5}
+
+p list.all? {|e| e > 5}
+
+p list.any? {|e| e == 2}
+
+p list.any? {|e| e == 5}
+
+p list.filter {|e| e.even?}
